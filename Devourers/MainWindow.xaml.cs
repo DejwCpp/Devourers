b7610645a800618c92f9279bdf4f6901a169fb9b
DejwCpp <dejwcpp@gmail.com>
2024-12-10 22:09:55 +0100

    Initial commit

---
+++ Devourers/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Devourers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MenuLogicUI _menuLogic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeMenuLogicUI();
+        }
+
+        private void InitializeMenuLogicUI()
+        {
+            _menuLogic = new MenuLogicUI(PlayerOneLabel, PlayerTwoLabel, PlayerOneButtonContainer, PlayerTwoButtonContainer);
+
+            foreach (UIElement element in PlayerOneButtonContainer.Children)
+            {
+                if (element is Button button)
+                {
+                    button.Click += _menuLogic.PlayerOneButton_Clicked;
+                    button.MouseEnter += _menuLogic.PlayerOneButton_MouseEnter;
+                    //button.MouseLeave += _menuLogic.PlayerOneButton_MouseLeave;
+                }
+            }
+
+            foreach (UIElement element in PlayerTwoButtonContainer.Children)
+            {
+                if (element is Button button)
+                {
+                    button.MouseEnter += _menuLogic.PlayerTwoButton_MouseEnter;
+                    //button.MouseLeave += _menuLogic.PlayerTwoButton_MouseLeave;
+                }
+            }
+        }
+    }
+}
---
+++ Devourers/MenuLogicUI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Devourers
+{
+    class MenuLogicUI
+    {
+        private Label _playerOneLabel;
+        private Label _playerTwoLabel;
+
+        private UniformGrid _playerOneButtons;
+        private UniformGrid _playerTwoButtons;
+
+        public MenuLogicUI(Label playerOneLabel, Label playerTwoLabel, UniformGrid playerOneButtons, UniformGrid playerTwoButtons)
+        {
+            _playerOneLabel = playerOneLabel;
+            _playerTwoLabel = playerTwoLabel;
+            _playerOneButtons = playerOneButtons;
+            _playerTwoButtons = playerTwoButtons;
+        }
+
+        public void PlayerOneButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button clickedButton)
+            {
+                foreach (var child in _playerOneButtons.Children)
+                {
+                    if (child is Button button)
+                    {
+                        // Reset the border for all buttons
+                        button.BorderThickness = new Thickness(0);
+                        button.BorderBrush = null;
+                    }
+                }
+                // Set the border for clicked button
+                clickedButton.BorderThickness = new Thickness(10);
+                clickedButton.BorderBrush = Brushes.Black;
+            }
+        }
+
+        public void PlayerOneButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (sender is Button button && button.Background is SolidColorBrush brush)
+            {
+                _playerOneLabel.Foreground = brush;
+            }
+        }
+
+        public void PlayerOneButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            _playerOneLabel.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        public void PlayerTwoButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (sender is Button button && button.Background is SolidColorBrush brush)
+            {
+                _playerTwoLabel.Foreground = brush;
+            }
+        }
+
+        public void PlayerTwoButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            _playerTwoLabel.Foreground = new SolidColorBrush(Colors.White);
+        }
+    }
+}
